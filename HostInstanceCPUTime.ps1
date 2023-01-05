a1ed52577b0e3e4873cc735be57c6af3c0dff804
owanvik <31723911+owanvik@users.noreply.github.com>
2023-01-05 12:39:07 +0100

    Add files via upload

---
+++ HostInstanceCPUTime.ps1
@@ -0,0 +1,25 @@
+﻿#This script gets the CPUTime for each BizTalk service and exports it to C:\ESB\Tools\HostInstancesCPUTime.json
+$i = 0
+DO {
+$i++
+$servicepid = Get-CimInstance -ClassName win32_service | Where-Object "State" -like 'Running' | Where-Object "Name" -like 'BTS*'| Select ProcessId, Name
+$process = gwmi Win32_PerfFormattedData_PerfProc_Process
+
+[System.Collections.ArrayList]$ArrayWithHeader = @()
+
+
+ForEach ($spid in $servicepid.processid) {
+$servicepid | Where-Object "ProcessId" -like $spid | Select ProcessId, Name | Out-Null
+$pprocess = $process | Where-Object -Property "IdProcess" -Like $spid | Select PercentProcessorTime
+$pname = $servicepid | Where-Object "ProcessId" -like $spid | Select Name
+$ppid = $servicepid | Where-Object "ProcessId" -like $spid | Select ProcessId
+
+$values = [pscustomobject] @{
+   'Name' = $pname.Name; 'ProcessId' = $ppid.ProcessId; 'CPUTime' = $pprocess.PercentProcessorTime
+}
+$ArrayWithHeader.Add($values) | Out-Null
+}
+$ArrayWithHeader | ConvertTo-Json  | Out-File "C:\ESB\Tools\HostInstancesCPUTime.json" -Encoding ascii -Force
+
+Start-Sleep -Seconds 25
+} While ($i -le 8)
